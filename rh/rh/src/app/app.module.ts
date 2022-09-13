3f99817bae0820ec7661bcd048931d2944a2e5e9
jppaivasilva <jppaivasilva@gmail.com>
2022-09-12 21:11:18 -0300

    On branch master  Your branch is up to date with 'origin/master'.

--- rh/rh/src/app/app.module.ts
+++ rh/rh/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadPessoaComponent } from './cad-pessoa/cad-pessoa.component';
-import { TemporarioComponent } from './temporario/temporario.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     CadPessoaComponent,
-    TemporarioComponent,
 ],
   imports: [
     BrowserModule,
