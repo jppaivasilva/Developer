ec956aa0caef013fe4ebd3abffe1d64c76cadc43
jppaivasilva <jppaivasilva@gmail.com>
2022-09-25 13:20:28 -0300

    25/09/2022 - 13:20

--- hospital/HCM/src/app/app-routing.module.ts
+++ hospital/HCM/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadpacienteComponent } from './cadpaciente/cadpaciente.component';
+import { ListapacienteComponent } from './listapaciente/listapaciente.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'lista', component: ListapacienteComponent},
+  {path: 'cadastro', component: CadpacienteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
